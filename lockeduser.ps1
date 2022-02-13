c6aec461b305b9266dcdb64096eb70aa9b43302a
Thiago Henrique Gomes <48591555+thethgomes@users.noreply.github.com>
2022-02-13 09:25:12 -0300

    Add files via upload

---
+++ lockeduser.ps1
@@ -0,0 +1,9 @@
+#Getting User locked event from Active Directory, ID: 4740
+$A = Get-WinEvent -MaxEvents 1 -FilterHashTable @{LogName='Security';ID='4740'} | Select-Object -Property *
+$EmailFrom = "dc-contoso@contoso.com"
+$EmailTo = "tilockedout@contoso.com"
+$Subject = "Usuario Bloqueado!!!"
+$Body = $A 
+# you may need to inform local mail server IP address from your exchange server
+$SMTPServer = "192.168.0.1"
+Send-MailMessage -From $EmailFrom -To $EmailTo -Subject $Subject -body ($Body | Out-String) -SmtpServer $SMTPServer
